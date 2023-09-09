667bec202b5b06f503eca0fc1738de13ae9e2a55
siMon <zz6210zz@163.com>
2023-09-09 22:20:12 +0800

    ci: add ci.yml

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,7 @@ module.exports = {
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module",
-        "project": "./tsconfig.eslint.json"
+        "project": "tsconfig.eslint.json"
     },
     "plugins": ["prettier"],
     "rules": {
